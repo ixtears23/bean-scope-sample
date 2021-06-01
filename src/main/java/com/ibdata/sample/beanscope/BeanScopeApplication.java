f514427ba1d34f4132bdae0fa8413b72c1d7a879
연구개발팀장 <ojs@ibdata.co.kr>
2021-06-01 09:42:50 +0900

    first

---
+++ src/main/java/com/ibdata/sample/beanscope/BeanScopeApplication.java
@@ -0,0 +1,13 @@
+package com.ibdata.sample.beanscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanScopeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibdata/sample/beanscope/BeanScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibdata.sample.beanscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanScopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
